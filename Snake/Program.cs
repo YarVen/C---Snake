1093ec74bbe51c7488516ade9bcdd3e50a649ceb
YarVen <drvy.mailbox@gmail.com>
2017-01-13 01:52:34 +0200

    program commit

---
+++ Snake/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Snake
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.SetBufferSize(120, 30);
+
+            Walls walls = new Walls(120, 30);
+            walls.Draw();
+        
+            //Отрисовка точек
+            Point p = new Point(4, 5, '*');
+            Snake snake = new Snake (p, 4, Direction.RIGHT);
+            snake.Draw();
+
+            FoodCreator foodCreator = new FoodCreator(80, 25, '$');
+            Point food = foodCreator.CreateFood();
+            food.Draw();
+
+ 
+            while(true)
+            {
+                if (walls.IsHit(snake) || snake.IsHitTail())
+                {
+                    break;
+                }
+                if(snake.Eat(food))
+                {
+                    food = foodCreator.CreateFood();
+                    food.Draw();
+                }
+                else
+                {
+                    snake.Move();
+                }
+
+                Thread.Sleep(100);
+
+                if (Console.KeyAvailable)
+                {
+                    ConsoleKeyInfo key = Console.ReadKey();
+                    snake.HandleKey(key.Key);
+                }
+            }
+        }
+
+        static void Draw(Figure figure)
+        {
+            figure.Draw();
+        }
+    }
+}
